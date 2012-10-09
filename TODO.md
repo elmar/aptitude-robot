31c6c3e71253c2c73affd6d4e22e0cc66a6d64d4
Elmar S. Heeb <elmar@heebs.ch>
2012-10-09 18:53:22 +0200

    added Debian packaging infrastructure

--- TODO.md
+++ TODO.md
@@ -6,3 +6,7 @@
   * empty package list (i.e. full-upgrade as default)
 
 * maybe generate a package list from an installed system
+
+* possibly sleep for a random number of seconds in cron.daily
+  this may spread the load between multiple hosts all running
+  aptitude-robot at the same time
---
+++ debian/init.d
@@ -0,0 +1,62 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          aptitude-robot
+# Required-Start:    $network $local_fs $remote_fs
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:
+# Short-Description: performs upgrades/installs/removes of software packages
+# Description:       calls aptitude full-upgrade with additional
+#                    parameters indicating software packages to be
+#                    installed/removed/purged on top of the regular
+#                    upgrade
+### END INIT INFO
+
+# Author: Elmar S. Heeb <elmar@heebs.ch>
+
+PATH=/sbin:/usr/sbin:/bin:/usr/bin
+DESC=aptitude-robot
+NAME=aptitude-robot
+SCRIPT=/usr/share/aptitude-robot/aptitude-robot-session
+SCRIPTNAME=/etc/init.d/$NAME
+
+# Exit if the package is not installed
+[ -x $SCRIPT ] || exit 0
+
+# Read configuration variable file if it is present
+[ -r /etc/default/$NAME ] && . /etc/default/$NAME
+
+# Load the VERBOSE setting and other rcS variables
+. /lib/init/vars.sh
+
+# Define LSB log_* functions.
+# Depend on lsb-base (>= 3.0-6) to ensure that this file is present.
+. /lib/lsb/init-functions
+
+case "$1" in
+  start)
+    [ "$RUN_ON_BOOT" = "no" ] && exit 0
+    [ "$VERBOSE" != no ] && log_daemon_msg "Running $DESC " "$NAME"
+    if fuser /var/lib/dpkg/lock > /dev/null 2> /dev/null ; then
+        if [ "$VERBOSE" != no ]; then
+            log_progress_msg "INFO: aptitude-robot init.d preventing recursive call while updating"
+            log_end_msg 0
+        fi
+        exit 0
+    fi
+    $SCRIPT
+    case "$?" in
+        0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
+        2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
+    esac
+  ;;
+  stop|restart|force-reload)
+    [ "$VERBOSE" != no ] && log_daemon_msg "Stop/restart/force-reload $DESC (does nothing)" "$NAME"
+  ;;
+  *)
+    echo "Usage: $SCRIPTNAME start" >&2
+    exit 3
+    ;;
+esac
+
+:
